a8783794feb07b78eda76eb38cb16e902d20f18b
Maksouni <max1205325@gmail.com>
2024-10-29 17:58:55 +0700

    Добавьте файлы проекта.

---
+++ HashPasswords/HashPassword.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPasswords
+{
+    public class HashPassword
+    {
+        public string HashPass(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] source = Encoding.UTF8.GetBytes(password);
+                byte[] hashSource = sha256.ComputeHash(source);
+                string hashPass = BitConverter.ToString(hashSource).Replace("-", String.Empty);
+                return hashPass;
+            }
+
+
+        }
+    }
+}
