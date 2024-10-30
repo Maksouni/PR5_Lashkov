730532122e354c5a3dd73b55278266f7c4525961
Maksouni <max1205325@gmail.com>
2024-10-30 15:42:24 +0700

    model added

---
+++ PR5_Lashkov/Helper.cs
@@ -0,0 +1,21 @@
+﻿using PR5_Lashkov.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR5_Lashkov
+{
+    internal class Helper
+    {
+        private static DrinkFactoryEntities _context;
+
+        public static DrinkFactoryEntities GetContext()
+        {
+            if(_context == null) 
+                _context = new DrinkFactoryEntities();
+            return _context;
+        }
+    }
+}
--- PR5_Lashkov/Program.cs
+++ PR5_Lashkov/Program.cs
@@ -12,7 +12,6 @@ namespace PR5_Lashkov
     {
         static void Main(string[] args)
         {
-            DbConnector db = new DbConnector();
 
             Console.WriteLine("Add new user\n");
             bool continueAdding = true;
@@ -27,8 +26,7 @@ namespace PR5_Lashkov
                 Console.Write("Enter role id: ");
                 int roleId = Convert.ToInt32(Console.ReadLine());
 
-                User user = new User(1, login, password, roleId);
-                db.AddUser(user);
+
 
                 
                 while (true)
@@ -45,11 +43,6 @@ namespace PR5_Lashkov
                 
             }
 
-            //User user = db.GetUserById(1);
-            //if (user != null )
-            //{
-            //    Console.WriteLine($"id: {user.Id} \nlogin: {user.Login} \npassword: {user.Password} \nrole: {user.Role}");
-            //}
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
